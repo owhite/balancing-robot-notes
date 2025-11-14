580bbb8637634dfa4246453e044feeef9de4b760
Owen White <smouldering.dog@gmail.com>
2025-11-13 19:18:22 -0500

    working on ICM42688

---
+++ teensy40/ICM42688_test/src/main.cpp
@@ -0,0 +1,62 @@
+#include "ICM42688.h"
+
+static const uint8_t CS_PIN  = 18;
+static const uint8_t INT_PIN = 19;
+
+ICM42688 imu(SPI, CS_PIN);
+volatile bool dataReady = false;
+
+void setImuFlag() { dataReady = true; }
+
+void setup() {
+  // serial to display data
+  Serial.begin(115'200);
+  while (!Serial) {}
+
+  // start communication with IMU
+  int status = imu.begin();
+  if (status < 0) {
+    Serial.println("IMU initialization unsuccessful");
+    Serial.println("Check IMU wiring or try cycling power");
+    Serial.print("Status: ");
+    Serial.println(status);
+    while (1) {}
+  }
+
+  // attaching the interrupt to micro controller pin INT_PIN
+  pinMode(INT_PIN, INPUT);
+  attachInterrupt(INT_PIN, setImuFlag, RISING);
+
+  // set output data rate to 12.5 Hz
+  imu.setAccelODR(ICM42688::odr12_5);
+  imu.setGyroODR(ICM42688::odr12_5);
+
+  // enabling the data ready interrupt
+  imu.enableDataReadyInterrupt();
+
+  Serial.println("ax,ay,az,gx,gy,gz,temp_C");
+}
+
+void loop() {
+  if (!dataReady) { return; }
+
+  dataReady = false;
+
+  // read the sensor
+  imu.getAGT();
+
+  // display the data
+  Serial.print(imu.accX(), 6);
+  Serial.print("\t");
+  Serial.print(imu.accY(), 6);
+  Serial.print("\t");
+  Serial.print(imu.accZ(), 6);
+  Serial.print("\t");
+  Serial.print(imu.gyrX(), 6);
+  Serial.print("\t");
+  Serial.print(imu.gyrY(), 6);
+  Serial.print("\t");
+  Serial.print(imu.gyrZ(), 6);
+  Serial.print("\t");
+  Serial.println(imu.temp(), 6);
+}
